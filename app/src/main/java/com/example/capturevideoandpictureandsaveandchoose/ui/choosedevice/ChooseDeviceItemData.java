abb45380c1927da5a0773849ab2deb06934b2db1
a12342346 <a12342346@gmail.com>
2020-04-08 15:45:53 +0800

    新增選擇設備data

---
+++ app/src/main/java/com/example/capturevideoandpictureandsaveandchoose/ui/choosedevice/ChooseDeviceItemData.java
@@ -0,0 +1,13 @@
+package com.example.capturevideoandpictureandsaveandchoose.ui.choosedevice;
+
+public class ChooseDeviceItemData {
+    private String recordDate="";
+    private String maintenancePlant="";
+    private String plant="";
+    private String productionPlant="";
+    private String deviceCategory="";
+    private String deciceId="";
+    private String keynote="";
+    private String filePath="";
+    private String uploadEmployees="";
+}
